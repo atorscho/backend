5589a6398200856cb86cf94aa6798a9802a0d0ab
Alex Torscho <contact@alextorscho.com>
2014-11-18 11:47:05 +0100

    Gulp tasks

--- public/assets/js/brand.js
+++ public/assets/js/brand.js
@@ -26,23 +26,50 @@ jQuery(document).ready(function ($) {
 			down: "fa fa-arrow-down"
 		}
 	});
+	$('.date').datetimepicker({
+		pickTime: false,
+		icons: {
+			time: "fa fa-clock-o",
+			date: "fa fa-calendar",
+			up: "fa fa-arrow-up",
+			down: "fa fa-arrow-down"
+		}
+	});
+	$('.time').datetimepicker({
+		pickDate: false,
+		icons: {
+			time: "fa fa-clock-o",
+			date: "fa fa-calendar",
+			up: "fa fa-arrow-up",
+			down: "fa fa-arrow-down"
+		}
+	});
 
 	/*
 	 * Dropdown Box
 	 */
-	$('.drop').click(function (e) {
+	//$('.drop > a').click(function (e) {
+	//	e.preventDefault();
+	//});
+	// todo - Prevent from opening multiple drop-boxes
+	$('.drop').on('click', function (e) {
 		// Prevent from opening multiple drop-boxes
-		$('.drop').removeClass('active');
-		$('.drop .drop-box').hide();
+		/*$('.drop').removeClass('active');
+		$('.drop .drop-box').hide();*/
 
 		$('.drop-box', this).slideDown('fast');
 		$(this).addClass('active');
 
 		e.stopPropagation();
 	});
-	$(document).click(function () {
-		$('.drop').removeClass('active');
-		$('.drop .drop-box').slideUp('fast');
+	$(document).click(function (e) {
+		var $drop = $('.drop');
+
+		if ( !$drop.is(e.target) && $drop.has(e.target).length === 0 )
+		{
+			$('.drop').removeClass('active');
+			$('.drop .drop-box').slideUp('fast');
+		}
 	});
 
 	/*
